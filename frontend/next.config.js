8a339a3075e79c8d5db0912b12c968764a1a42f5
Jean Carlo Semprit Rodriguez <jean.semprit@hotmail.com>
2023-08-14 20:34:27 -0400

    a lot of trials and error but finally make it work :D

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
 
-module.exports = nextConfig;
+module.exports = {
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
